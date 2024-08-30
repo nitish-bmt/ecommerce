6583cacf6d349ba4dc6b74a21a729ac38b8b455b
Nitish <nitish.rawat@binmile.com>
2024-08-30 11:53:45 +0530

    removed distribution folder

--- notificationService/dist/controllers/ordersController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.placeOrder = placeOrder;
-function placeOrder(req, res) {
-    res.send("order placed");
-}
--- orderService/dist/utils/healthcheck.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formattedServerUptime = formattedServerUptime;
-function formattedServerUptime() {
-    const uptime = process.uptime();
-    return `${Math.floor(uptime / (60 * 60))}hrs ${Math.floor((uptime % (60 * 60)) / 60)}mins ${Math.floor(uptime % 60)}secs`;
-}
